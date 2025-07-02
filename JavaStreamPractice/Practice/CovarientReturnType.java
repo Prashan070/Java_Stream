c4280deb6e250ac5738e0f8c1b870a0aebfa9626
Prashan070 <prashansrivastava77@gmail.com>
2025-07-02 05:47:14 +0530

    Initail 2

--- JavaStreamPractice/Practice/CovarientReturnType.java
+++ JavaStreamPractice/Practice/CovarientReturnType.java
@@ -1,4 +1,30 @@
 package JavaStreamPractice.Practice;
 
+
+class Animal {
+
+}
+
+class Dog extends Animal {
+
+}
+
+class Parent {
+    Animal display() {
+        return new Animal();
+    }
+}
+
+class child extends Parent {
+    @Override
+    Dog display() {
+        return new Dog();
+    }
+}
+
+
 public class CovarientReturnType {
+    public static void main(String[] args) {
+
+    }
 }
--- JavaStreamPractice/Practice/GenericClassAndMethod.java
+++ JavaStreamPractice/Practice/GenericClassAndMethod.java
@@ -1,4 +1,40 @@
 package JavaStreamPractice.Practice;
 
+class GenClass<K> {
+    K val;
+
+    public GenClass(K val) {
+        this.val = val;
+    }
+
+    public K getVal() {
+        return val;
+    }
+
+    public void setVal(K val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return "GenClass{" +
+                "val=" + val +
+                '}';
+    }
+
+    //the method is static, it cannot use class-level generics (like K in GenClass<K>).
+    K display() {
+        return val;
+    }
+}
+
+
 public class GenericClassAndMethod {
+    public static void main(String[] args) {
+
+        GenClass<String> s1 = new GenClass<>("Prashan");
+        GenClass<Integer> s2 = new GenClass<>(1);
+
+
+    }
 }
