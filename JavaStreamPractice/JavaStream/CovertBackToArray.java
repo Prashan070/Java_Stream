0b1e9340217a8430be3e8960b8bf33422e97d719
Prashan070 <prashansrivastava77@gmail.com>
2025-07-05 00:44:21 +0530

    Initail 8

--- JavaStreamPractice/JavaStream/CovertBackToArray.java
+++ JavaStreamPractice/JavaStream/CovertBackToArray.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class CovertBackToArray {
     public static void main(String[] args) {
         Stream<String> stream = Stream.of("apple", "banana", "cherry");
-        
+
         String[] array = stream.toArray(String[]::new);
 
     }
